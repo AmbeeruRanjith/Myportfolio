1633d5dc4bf989129910feb7e14cf1e31945e2e9
ranji <ranjithambeeru21@gmail.com>
2025-04-25 14:35:28 +0530

    profile summary changed

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Ranjith</span><br/>Website Designer</span>
             <p className="introPara">
-          I'm a skilled Web and Android app developer with experience in building 
+          I'm a skilled Web and Android app development with experience in building 
           responsive, user-friendly websites and mobile apps using modern tools 
           like React, Kotlin, and Firebase. I focus on delivering clean UI, 
           real-time functionality, and scalable solutions.
